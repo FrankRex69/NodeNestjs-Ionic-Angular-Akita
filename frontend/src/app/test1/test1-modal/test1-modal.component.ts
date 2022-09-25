2c035144f9981f468985f3480da6fd5904bb98c7
FrankRex69 <48279165+FrankRex69@users.noreply.github.com>
2022-09-25 20:16:45 +0200

    Test interfaces (#2)
    * feat: base setup
    
    * gitignore
    
    * feat: fileds into modal test1
    
    * fix: quick fix
    
    * feat: create record in modal
    
    * feat: change button with ion-button
    
    * fix: clear
    
    * feat: link into home

--- frontend/src/app/test1/test1-modal/test1-modal.component.ts
+++ frontend/src/app/test1/test1-modal/test1-modal.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import {
-ModalController,
-NavParams
-} from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IresponseTest1 } from '@commons/interfaces/test1.interface';
+import { ModalController, NavParams } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { Test1Service } from '../test1.service';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -13,13 +13,17 @@ NavParams
 // eslint-disable-next-line @angular-eslint/component-class-suffix
 export class Test1Modal implements OnInit {
 
-
   modalTitle: string;
   modelId: number;
 
+  createTest1Sub: Subscription;
+  isCreateActivated: boolean;
+  test1ToBeCreated: IresponseTest1;
+
   constructor(
     private modalController: ModalController,
-    private navParams: NavParams
+    private navParams: NavParams,
+    private test1Service: Test1Service
   ) { }
 
   ngOnInit() {
@@ -32,6 +36,13 @@ export class Test1Modal implements OnInit {
     // eslint-disable-next-line @typescript-eslint/no-inferrable-types
     const onClosedData: string = 'Wrapped Up!';
     await this.modalController.dismiss(onClosedData);
+  }
+
+  createTest1(createForm) {
+    console.log(createForm.value.campo1);
+    console.log(createForm.value.campo2);
+    this.createTest1Sub = this.test1Service.createTest1(createForm).subscribe(result => {});
+    this.closeModal();
   }
 
 }
--- frontend/src/app/test1/test1.module.ts
+++ frontend/src/app/test1/test1.module.ts
@@ -7,6 +7,7 @@ import { Test1PageRoutingModule } from './test1-routing.module';
 import { Test1Page } from './test1.page';
 import { Test1Service } from './test1.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Test1Modal } from './test1-modal/test1-modal.component';
 
 @NgModule({
   imports: [
@@ -16,7 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
     Test1PageRoutingModule,
     HttpClientModule
   ],
-  declarations: [Test1Page],
+  declarations: [Test1Page, Test1Modal],
   providers: [Test1Service],
   exports: [Test1Page]
 })
