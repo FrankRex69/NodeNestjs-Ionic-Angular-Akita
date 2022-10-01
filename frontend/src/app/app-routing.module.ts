44074be27a567e1cbf04fa67a342e16b43ef2074
FrankRex69 <francesco.re69@gmail.com>
2022-10-01 08:40:24 +0200

    fix: quick fix

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,17 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'test1',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
     path: 'test1',
-    loadChildren: () => import('./test1/test1.module').then( m => m.Test1PageModule)
+    loadChildren: () => import('./test1/test1.module').then( m => m.Test1PageModule),
   },
 ];
 
